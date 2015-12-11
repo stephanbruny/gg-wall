412c6c8dac059e843334c52400ea432f715cfeec
Stephan Bruny <stephan.bruny@live.de>
2015-12-11 16:47:09 +0100

    Get wallpaper directory from argv. Added shuffling through files

--- src/wall.py
+++ src/wall.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import gtk
 import os
 import cairo
 import Image
 import StringIO
 import glib
+import sys
 
 class gg_wall(gtk.Window):
 	def __init__(self):
@@ -34,9 +35,19 @@ class gg_wall(gtk.Window):
 
 		self.wallpaper = gtk.Image()
 		self.surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
+		self.wp_files = []
+		self.wp_dir = ''
+		self.wp_index = 0
+		self.time_out = None
+
+	def set_directory(self, path):
+		self.wp_dir = path
+		self.wp_files = [f for f in os.listdir(self.wp_dir) if os.path.isfile(os.path.join(self.wp_dir, f))]
+		self.set_wallpaper(self.wp_files[self.wp_index])
+
 
 	def set_wallpaper(self, path):
-		img = Image.open(path)
+		img = Image.open(os.path.join(self.wp_dir, path))
 		buf = StringIO.StringIO()
 		img.save(buf, format="PNG")
 		buf.seek(0)
@@ -46,17 +57,29 @@ class gg_wall(gtk.Window):
 		cr = widget.window.cairo_create()
 		cr.set_source_surface(self.surface, 0, 0)
 		cr.paint()
-		# cr.restore()
+	
+	def shuffle(self):
+		if self.wp_index < (len(self.wp_files) - 1):
+			self.wp_index = self.wp_index + 1
+		else:
+			self.wp_index = 0
+		self.set_wallpaper(self.wp_files[self.wp_index])
+		self.queue_draw()
+		return True
 
+	def set_shuffle(self, msec):
+		if self.time_out is not None:
+			glib.source_remove(self.time_out)
+		self.time_out = glib.timeout_add(msec, self.shuffle)
 
-wallpaper_dir = "/home/odroid/Pictures/Wallpapers"
+if len(sys.argv) >= 2:
+	wallpaper_dir = sys.argv[1] # "/home/odroid/Pictures/Wallpapers"
 
-files = [f for f in os.listdir(wallpaper_dir) if os.path.isfile(os.path.join(wallpaper_dir, f))]
+	win = gg_wall()
+	win.set_directory(wallpaper_dir)
+	win.set_shuffle(5000)
+	win.show_all()
+	gtk.main()
+else:
+	print("gg-wall needs a dirtectory to read wallpaper files from\nUsage: gg-wall <path>\n")
 
-wallpaper_path = os.path.join(wallpaper_dir, files[1])
-
-win = gg_wall()
-win.set_wallpaper(wallpaper_path)
-win.show_all()
-
-gtk.main()
